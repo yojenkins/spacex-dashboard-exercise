18950d3e66a091967540b00d6ad145736d16dcc3
Jenkins <evan.mailto@gmail.com>
2022-02-12 10:09:53 -0800

    feat: basic dark mode toggle

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { useState } from "react";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [mode, setMode] = useState(false);
+  const toggleMode = () => setMode((m) => !m);
+  return (
+    <div className={`${mode ? "dark" : ""} h-full`}>
+      <main className="bg-gray-50 dark:bg-gray-800 h-full">
+        <button onClick={toggleMode}>Dark Mode</button>
+        <Component {...pageProps} />
+      </main>
+    </div>
+  );
 }
-export default MyApp
+export default MyApp;
