3690f37200cd9dcd493c8090f22a2de314dd89a1
Jenkins <evan.mailto@gmail.com>
2022-02-23 22:13:58 -0800

    chore: substitute font family

--- components/common/typography/Heading.tsx
+++ components/common/typography/Heading.tsx
@@ -1,7 +1,11 @@
 import React, { FC } from "react";
 
 const Heading: FC = ({ children }) => {
-  return <h1 className="text-xl text-black dark:text-white">{children}</h1>;
+  return (
+    <h1 className="text-xl text-black dark:text-white font-display font-bold">
+      {children}
+    </h1>
+  );
 };
 
 export default Heading;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,8 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head />
-      <body className="bg-gray-50 dark:bg-gray-800">
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&family=Poppins:wght@500;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body className="bg-gray-50 dark:bg-gray-800 font-body">
         <Main />
         <NextScript />
       </body>
