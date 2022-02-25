8dab0689fb3ad2990031d1586722195a3f850072
Jenkins <evan.mailto@gmail.com>
2022-02-24 21:30:55 -0800

    refactor: separate payloads graph pieces

--- components/pages/dashboard/PayloadsByNationality.tsx
+++ components/pages/dashboard/PayloadsByNationality.tsx
@@ -18,7 +18,7 @@ const TableHeading: FC = ({ children }) => (
   <Text className="uppercase font-display">{children}</Text>
 );
 
-function PayloadsByNationality({ data }: Props) {
+export default function PayloadsByNationality({ data }: Props) {
   return (
     <Card>
       <Card.Header>
@@ -38,7 +38,7 @@ function PayloadsByNationality({ data }: Props) {
                   paddingAngle={5}
                   cornerRadius={10}
                 >
-                  {data.map((entry, index) => (
+                  {data.map((_, index) => (
                     <Cell
                       key={`cell-${index}`}
                       fill={COLORS[index % COLORS.length]}
@@ -49,44 +49,7 @@ function PayloadsByNationality({ data }: Props) {
             </ResponsiveContainer>
           </div>
           <div className="flex-1">
-            <ul>
-              <li className="py-2 flex">
-                <div className="w-64">
-                  <TableHeading>Nationality</TableHeading>
-                </div>
-                <div className="flex-1">
-                  <TableHeading>Value</TableHeading>
-                </div>
-              </li>
-              {data.slice(0, 5).map(({ name, count }, index) => (
-                <li
-                  className={`${
-                    index !== 0
-                      ? "border-t-2 border-gray-50 dark:border-gray-800"
-                      : ""
-                  } py-2 flex`}
-                  key={name}
-                >
-                  <div className="w-64">
-                    <svg
-                      className="inline-block mr-2"
-                      viewBox="0 0 100 100"
-                      width="8"
-                      height="8"
-                      fill={COLORS[index]}
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <circle cx="50" cy="50" r="50" />
-                    </svg>
-
-                    <Text>{name}</Text>
-                  </div>
-                  <div className="flex-1">
-                    <Text>{count}</Text>
-                  </div>
-                </li>
-              ))}
-            </ul>
+            <Legend data={data.slice(0, 5)} />
           </div>
         </div>
       </Card.Body>
@@ -94,4 +57,53 @@ function PayloadsByNationality({ data }: Props) {
   );
 }
 
-export default PayloadsByNationality;
+const Legend = ({ data }: Props) => {
+  return (
+    <ul>
+      <li className="py-2 flex">
+        <div className="w-64">
+          <TableHeading>Nationality</TableHeading>
+        </div>
+        <div className="flex-1">
+          <TableHeading>Value</TableHeading>
+        </div>
+      </li>
+      {data.slice(0, 5).map((row, index) => (
+        <LegendItem row={row} index={index} key={row.name} />
+      ))}
+    </ul>
+  );
+};
+
+const LegendItem = ({
+  row,
+  index,
+}: {
+  row: NationalityCount;
+  index: number;
+}) => {
+  const { name, count } = row;
+  const firstItemCls = "border-t-2 border-gray-50 dark:border-gray-800";
+
+  return (
+    <li className={`${index !== 0 && firstItemCls} py-2 flex`}>
+      <div className="w-64">
+        <svg
+          className="inline-block mr-2"
+          viewBox="0 0 100 100"
+          width="8"
+          height="8"
+          fill={COLORS[index]}
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <circle cx="50" cy="50" r="50" />
+        </svg>
+
+        <Text>{name}</Text>
+      </div>
+      <div className="flex-1">
+        <Text>{count}</Text>
+      </div>
+    </li>
+  );
+};
